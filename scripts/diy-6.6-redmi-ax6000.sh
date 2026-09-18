7146734de6bd3a74cc8ab859df05e2da3481841a
Sky2016cn <58769170+sky2016cn@users.noreply.github.com>
2026-09-19 06:55:28 +0800

    Update diy-6.6-redmi-ax6000.sh

--- scripts/diy-6.6-redmi-ax6000.sh
+++ scripts/diy-6.6-redmi-ax6000.sh
@@ -2,15 +2,15 @@
 set -e -o pipefail
 
 WORKSPACE_ROOT="${GITHUB_WORKSPACE:-$(pwd)}"
-GOLANG126_SRC_DIR="$WORKSPACE_ROOT/scripts/6.6/golang1.26"
-GOLANG126_FEED_DIR="feeds/packages/lang/golang1.26"
+GOLANG126_SRC_DIR="$WORKSPACE_ROOT/scripts/6.6/golang1.27"
+GOLANG126_FEED_DIR="feeds/packages/lang/golang1.27"
 rm -rf "$GOLANG126_FEED_DIR"
 mkdir -p "$GOLANG126_FEED_DIR"
 cp -rf "$GOLANG126_SRC_DIR/." "$GOLANG126_FEED_DIR/"
 ./scripts/feeds update -f packages
-./scripts/feeds install golang1.26
+./scripts/feeds install golang1.27
 
-# passwall daed use golang1.26/host
+# passwall daed use golang1.27/host
 find package/dae package/passwall-packages -name "Makefile" -type f -exec sed -i \
-  -e 's|\<golang/golang-package.mk\>|golang1.26/golang-package.mk|g' \
-  -e 's|\<golang/host\>|golang1.26/host|g' {} +
+  -e 's|\<golang/golang-package.mk\>|golang1.27/golang-package.mk|g' \
+  -e 's|\<golang/host\>|golang1.27/host|g' {} +
